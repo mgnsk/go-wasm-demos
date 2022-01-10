de25bcbde7091e53373d1e54de96330e9e55e186
Magnus Kokk <magnus@kokk.eu>
2022-01-10 19:20:42 +0200

    benchmark: force performance.now(), pregenerate bytes

--- cmdwasm/benchmark/main.go
+++ cmdwasm/benchmark/main.go
@@ -4,7 +4,7 @@
 package main
 
 import (
-	"bytes"
+	"bufio"
 	"io"
 	"io/ioutil"
 	"time"
@@ -31,56 +31,78 @@ func main() {
 	}
 }
 
+// byteGenerator always reads a sequence of 1s.
+type byteGenerator struct{}
+
+func (byteGenerator) Read(b []byte) (int, error) {
+	for i := 0; i < len(b); i++ {
+		b[i] = 1
+	}
+	return len(b), nil
+}
+
 func browser() {
 	defer jsutil.ConsoleLog("Exiting main program")
 
 	jsutil.ConsoleLog("running echoBytes benchmark")
 
-	r, w := wrpc.Go("echoBytes")
-	go func() {
-		defer w.Close()
-		t := time.NewTimer(5 * time.Second)
-		for {
-			select {
-			case <-t.C:
-				return
-			default:
-				// write 64K bytes each time
-				if _, err := w.Write(bytes.Repeat([]byte{0}, 64000)); err != nil {
+	initialSize := 1 * 1024
+	maxSize := 1024 * 1024
+	dur := 2 * time.Second
+
+	for size := initialSize; size <= maxSize; size *= 2 {
+		r, w := wrpc.Go("echoBytes")
+		go func() {
+			defer w.Close()
+			rd := bufio.NewReaderSize(byteGenerator{}, size)
+			start := time.Now()
+			for {
+				if time.Since(start) > dur {
+					return
+				}
+				if _, err := io.CopyN(w, rd, int64(size)); err != nil {
 					panic(err)
 				}
 			}
+		}()
+
+		b, err := ioutil.ReadAll(r)
+		if err != nil {
+			panic(err)
 		}
-	}()
+		mps := (float64(len(b)) / dur.Seconds()) / 1024 / 1024
 
-	start := time.Now()
-	b, err := ioutil.ReadAll(r)
-	if err != nil {
-		panic(err)
+		jsutil.ConsoleLog("echoBytes %dK: MB/s:", size/1024, mps)
 	}
-	dur := time.Since(start)
-	mps := (float64(len(b)) / dur.Seconds()) / 1024 / 1024
-
-	jsutil.ConsoleLog("echoBytes: MB/s:", mps)
 
 	jsutil.ConsoleLog("running call benchmark")
 
-	r, w = wrpc.Go("call")
-	t := time.NewTimer(5 * time.Second)
+	initialConcurrency := 1
+	maxConcurrency := 32
 
-	n := 0
-	start = time.Now()
-	for {
-		select {
-		case <-t.C:
-			dur := time.Since(start)
-			ops := float64(n) / dur.Seconds()
-			jsutil.ConsoleLog("call: ops:", ops)
-			return
-		default:
-			_, w = wrpc.Go("call")
-			w.Close()
-			n++
+	for concurrency := initialConcurrency; concurrency <= maxConcurrency; concurrency *= 2 {
+		start := time.Now()
+		result := make(chan float64)
+		for i := 0; i < concurrency; i++ {
+			go func() {
+				n := 0
+				for {
+					if d := time.Since(start); d > 2*time.Second {
+						ops := float64(n) / d.Seconds()
+						result <- ops
+						break
+					}
+					wrpc.Go("call")
+					n++
+				}
+			}()
 		}
+
+		var ops float64
+		for i := 0; i < concurrency; i++ {
+			ops += <-result
+		}
+
+		jsutil.ConsoleLog("call: concurency %d: ops:", concurrency, ops)
 	}
 }
